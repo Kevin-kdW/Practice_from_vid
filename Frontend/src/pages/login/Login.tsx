39c12dc34525f56eb956adad9878ff829284c490
kevin-kdW <k.r.young192@gmail.com>
2023-06-14 18:11:41 +1200

    More updates

--- Frontend/src/pages/login/Login.tsx
+++ Frontend/src/pages/login/Login.tsx
@@ -1,9 +1,25 @@
-import { Heading, Input } from "@chakra-ui/react";
+import { Button, Heading, Input } from "@chakra-ui/react";
+import React,{ useState } from "react";
 
 export default function Login() {
-  return;
-  <div>
-    <Heading textAlign ={`center`}>Login</Heading>
-    <Input />
-  </div>;
+  const [userEmail, setUserEmail] = useState('');
+
+  console.log(userEmail.target.value);
+
+  const handleLogin = () => {
+    console.log("Logged in");
+  };
+
+  //if handleLogin needs to pass a parameter change to this '() => handleLogin(PARAMETER)'
+
+  return (
+    <div>
+      <Heading mb={10} onChange={setUserEmail}  textAlign={"center"}>
+        Login
+      </Heading>
+      <Input mb={10} type="email" placeholder="Enter email" />
+      <Input mb={10} type="password" placeholder="Enter password" />
+      <Button onClick={handleLogin}>Login</Button>
+    </div>
+  );
 }
